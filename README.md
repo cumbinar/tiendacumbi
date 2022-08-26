0412504ab9cda8f9928d226e5680f0347839894d
Eduardo Guevara Melo <96537681+cumbinar@users.noreply.github.com>
2022-08-25 19:14:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tiendacumbi
+ Tienda Cumbi
---
+++ src/main/java/com/tiendacumbi/TiendacumbiApplication.java
@@ -0,0 +1,13 @@
+package com.tiendacumbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendacumbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendacumbiApplication.class, args);
+	}
+
+}
