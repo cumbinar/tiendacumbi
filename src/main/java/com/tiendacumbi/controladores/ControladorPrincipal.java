1b55e60696f3cb1d8d1593b9173d8da7d1ac12e9
Eduardo Guevara Melo <96537681+cumbinar@users.noreply.github.com>
2022-08-27 19:59:54 -0500

    create controladorPincipal en controladores

---
+++ src/main/java/com/tiendacumbi/controladores/ControladorPrincipal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tiendacumbi.controladores;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author educu
+ */
+@Controller
+public class ControladorPrincipal {
+    public String cargarIndex(){
+    return "index";
+    }
+}
