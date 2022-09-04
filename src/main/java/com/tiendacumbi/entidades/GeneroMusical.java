e3f4c9987fceb511f98d865cda8c51ba4ed41125
Eduardo Guevara Melo <96537681+cumbinar@users.noreply.github.com>
2022-09-04 08:32:02 -0500

    create GeneroMusical class

---
+++ src/main/java/com/tiendacumbi/entidades/GeneroMusical.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tiendacumbi.entidades;
+
+/**
+ *
+ * @author educu
+ */
+public class GeneroMusical {
+    
+}
