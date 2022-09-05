9b6b738005d8127b33d9aa14cf603122137aceb0
Eduardo Guevara Melo <96537681+cumbinar@users.noreply.github.com>
2022-09-04 21:16:36 -0500

    extends jpa.repository

---
+++ src/main/java/com/tiendacumbi/repositorios/IGeneroRepository.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.tiendacumbi.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author educu
+ */
+public interface IGeneroRepository  extends JpaRepository{
+    
+}
