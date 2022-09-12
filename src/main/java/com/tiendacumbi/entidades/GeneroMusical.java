e9dd0321170599c725e986acbf65871bd72eb5b1
Eduardo Guevara Melo <96537681+cumbinar@users.noreply.github.com>
2022-09-12 16:30:02 -0500

    modify constructors GeneroMusical

--- src/main/java/com/tiendacumbi/entidades/GeneroMusical.java
+++ src/main/java/com/tiendacumbi/entidades/GeneroMusical.java
@@ -9,5 +9,60 @@ package com.tiendacumbi.entidades;
  * @author educu
  */
 public class GeneroMusical {
+    private int id;
+    private String nombre;
+    private String carpeta;
+    private boolean hab;
+
+    public GeneroMusical() {
+    }
+
+    public GeneroMusical(String nombre, String carpeta, boolean hab) {
+        this.nombre = nombre;
+        this.carpeta = carpeta;
+        this.hab = hab;
+    }
+
+    public GeneroMusical(int id, String nombre, String carpeta, boolean hab) {
+        this.id = id;
+        this.nombre = nombre;
+        this.carpeta = carpeta;
+        this.hab = hab;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCarpeta() {
+        return carpeta;
+    }
+
+    public void setCarpeta(String carpeta) {
+        this.carpeta = carpeta;
+    }
+
+    public boolean isHab() {
+        return hab;
+    }
+
+    public void setHab(boolean hab) {
+        this.hab = hab;
+    }
+    
+    
+    
     
 }
