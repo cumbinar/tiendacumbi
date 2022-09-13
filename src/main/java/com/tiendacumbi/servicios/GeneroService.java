f7b22c719b9f7514b6eebf36e2d7ef3bfadb51ba
Eduardo Guevara Melo <96537681+cumbinar@users.noreply.github.com>
2022-09-12 22:17:33 -0500

    create GeneroService

---
+++ src/main/java/com/tiendacumbi/servicios/GeneroService.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tiendacumbi.servicios;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author educu
+ */
+@Service
+public class GeneroService {
+    
+    public void crearNuevoGenero(){
+    
+    }
+    
+    public void actualizarGenero(){
+    
+    }
+    
+    public void consultarGeneros(){
+    
+    }
+    
+    public void eliminarGenero(){
+    
+    }
+    
+}
