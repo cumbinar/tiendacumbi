96cff4c7a12e52242ca9bd60c3bf0457e15f9d89
Eduardo Guevara Melo <96537681+cumbinar@users.noreply.github.com>
2022-09-14 17:26:49 -0500

    create methods controller html

---
+++ src/main/java/com/tiendacumbi/controladores/GenerosController.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tiendacumbi.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *index.html /
+ * listageneros.html /generos
+ * formgeneros.html /generos/form/{id}
+ * @author educu
+ */
+@Controller
+public class GenerosController {
+    
+    @GetMapping("/")
+    public String cargarIndex(){
+        return "index";
+    }
+    
+    @GetMapping("/generos")
+    public String cargarListaGeneros(){
+        return "listageneros";
+    }
+    
+    
+    @GetMapping("/formgeneros")
+    public String cargarFormGeneros(){
+        return "formgeneros";
+    }
+}
+
