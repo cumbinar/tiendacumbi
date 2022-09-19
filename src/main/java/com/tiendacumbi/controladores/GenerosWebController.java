120b2d13b38df94dc2abd3562ae6dac92b835044
Eduardo Guevara Melo <96537681+cumbinar@users.noreply.github.com>
2022-09-18 21:25:56 -0500

    create GenerosWebController

---
+++ src/main/java/com/tiendacumbi/controladores/GenerosWebController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tiendacumbi.controladores;
+
+import com.tiendacumbi.entidades.GeneroMusical;
+import com.tiendacumbi.servicios.GeneroService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author educu
+ */
+@RestController
+public class GenerosWebController {
+
+    @Autowired
+    private GeneroService servicio;
+
+    @GetMapping("/api/generos")
+    public ResponseEntity<List<GeneroMusical>> consultarGeneros() {
+        List<GeneroMusical> lista = servicio.consultarGeneros();
+        return new ResponseEntity<>(lista, HttpStatus.OK);
+    }
+}
