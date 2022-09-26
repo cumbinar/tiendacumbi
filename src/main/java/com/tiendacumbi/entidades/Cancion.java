3463dd223e18763ee39baddfc1357b4023c616bf
Eduardo Guevara Melo <96537681+cumbinar@users.noreply.github.com>
2022-09-26 07:28:05 -0500

    generated get and set

--- src/main/java/com/tiendacumbi/entidades/Cancion.java
+++ src/main/java/com/tiendacumbi/entidades/Cancion.java
@@ -44,6 +44,11 @@ public class Cancion {
     @JoinColumn(name = "idGeneroMusical")
     private GeneroMusical genero;    
 
+    public Cancion() {
+    }
+    
+    
+
     public Cancion(String nombre, int anioPublicacion, String compositor, String interprete, String imagen, GeneroMusical genero) {
         this.nombre = nombre;
         this.anioPublicacion = anioPublicacion;
@@ -60,6 +65,62 @@ public class Cancion {
         this.compositor = compositor;
         this.interprete = interprete;
         this.imagen = imagen;
+        this.genero = genero;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    public String getCompositor() {
+        return compositor;
+    }
+
+    public void setCompositor(String compositor) {
+        this.compositor = compositor;
+    }
+
+    public String getInterprete() {
+        return interprete;
+    }
+
+    public void setInterprete(String interprete) {
+        this.interprete = interprete;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public GeneroMusical getGenero() {
+        return genero;
+    }
+
+    public void setGenero(GeneroMusical genero) {
         this.genero = genero;
     }
     
