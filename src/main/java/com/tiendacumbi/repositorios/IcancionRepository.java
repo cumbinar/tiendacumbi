155b5ef8e4346677cec22901e16f01a4c473aa5b
Eduardo Guevara Melo <96537681+cumbinar@users.noreply.github.com>
2022-09-27 06:27:19 -0500

    Create IcancionRepository.java

---
+++ src/main/java/com/tiendacumbi/repositorios/IcancionRepository.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.tiendacumbi.repositorios;
+
+import com.tiendacumbi.entidades.Cancion;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author educu
+ */
+@Repository
+public interface IcancionRepository extends JpaRepository<Cancion, Integer>{
+    public List<Cancion> findByNombreContainingOrCompositorContainingOrInterpreteContaining(String c1, String c2, String c3);
+}
